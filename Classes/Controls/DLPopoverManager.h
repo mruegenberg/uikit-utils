5e7206f8b001d12ececf12155b213a93a1a7e3f4
Marcel Ruegenberg <dustlab@dustlab.com>
2014-03-17 18:48:02 +0100

    Added files

---
+++ Classes/Controls/DLPopoverManager.h
@@ -0,0 +1,22 @@
+//
+//  DLPopoverManager.h
+//  Lists
+//
+//  Created by Marcel Ruegenberg on 25.02.11.
+//  Copyright 2011 Dustlab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+/** 
+ A class for managing the ownership of popovers in a central place.
+
+ Because even though Apple basically tells you to have only one popover at a time, they make it surprisingly hard to do this properly.
+ */
+@interface DLPopoverManager : NSObject
+
+@property (nonatomic, strong) UIPopoverController *currentPopoverController;
+
++ (DLPopoverManager *)sharedPopoverManager;
+
+@end
---
+++ Classes/ViewUtil/UIView+Additions.h
@@ -0,0 +1,17 @@
+//
+//  UIView+Additions.h
+//  CheckLists
+//
+//  Created by Marcel Ruegenberg on 31.10.10.
+//  Copyright 2010 Dustlab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface UIView (DLAdditions)
+
+// find the current first responder in this view or its subviews
+- (UIView *)findFirstResponder;
+
+@end
